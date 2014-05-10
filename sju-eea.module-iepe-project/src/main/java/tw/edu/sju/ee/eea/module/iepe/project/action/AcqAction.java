b0c9426f36131784091a40bc74fb687b6aa329ca
薛聿明 <102m05008@stud.sju.edu.tw>
2014-05-10 15:40:29 +0800

    Action.

---
+++ sju-eea.module-iepe-project/src/main/java/tw/edu/sju/ee/eea/module/iepe/project/action/AcqAction.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2014 Leo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package tw.edu.sju.ee.eea.module.iepe.project.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import javax.swing.Action;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
+import org.openide.util.TaskListener;
+import tw.edu.sju.ee.eea.module.iepe.project.IepeProject;
+
+@ActionID(
+        category = "Acoustic",
+        id = "tw.edu.sju.ee.eea.module.iepe.project.object.AcqAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_AcqAction"
+)
+@ActionReference(path = "Menu/Analyzers", position = 500)
+@Messages("CTL_AcqAction=Acq")
+public final class AcqAction implements  ActionListener {
+
+    private final IepeProject context;
+    private final static RequestProcessor RP = new RequestProcessor("interruptible tasks", 1, true);
+    private ProgressHandle progr;
+
+    public AcqAction(IepeProject context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        // TODO use context
+        RequestProcessor.Task task = RP.create(context);
+        progr = ProgressHandleFactory.createHandle("Simple task", task);
+        task.addTaskListener(new TaskListener() {
+            public void taskFinished(org.openide.util.Task task) {
+                System.out.println("fin");
+                progr.finish();
+            }
+        });
+        progr.start();
+        task.schedule(0);
+    }
+
+//    public void run() {
+//        try {
+//
+//            progr.start();
+//            Thread.sleep(5000);
+//            progr.progress("List prepared");
+//            Thread.sleep(5000);
+//            progr.progress("Objects loaded");
+//            Thread.sleep(5000);
+//            progr.progress("Objects structure built");
+//
+//            progr.finish();
+//        } catch (InterruptedException ex) {
+//        }
+//    }
+
+}
---
+++ sju-eea.module-iepe-project/src/main/java/tw/edu/sju/ee/eea/module/iepe/project/action/ListenAction.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2014 Leo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package tw.edu.sju.ee.eea.module.iepe.project.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Collection;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.netbeans.api.project.Project;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
+import org.openide.util.TaskListener;
+import tw.edu.sju.ee.eea.module.iepe.project.IepeProject;
+import tw.edu.sju.ee.eea.module.iepe.project.IepeProject.IepeProjectLogicalView;
+import tw.edu.sju.ee.eea.module.iepe.project.object.IepeRealtimeObject;
+import tw.edu.sju.ee.eea.util.iepe.IEPEPlayer;
+
+@ActionID(
+        category = "IEPE",
+        id = "tw.edu.sju.ee.eea.module.iepe.project.object.ListenAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_ListenAction"
+)
+@ActionReference(path = "Menu/Analyzers", position = 400, separatorBefore = 350)
+@Messages("CTL_ListenAction=Listen")
+public final class ListenAction implements ActionListener {
+
+    private final IepeProject context;
+    private final static RequestProcessor RP = new RequestProcessor("interruptible tasks", 1, true);
+    private ProgressHandle progr;
+
+    public ListenAction(IepeProject context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        try {
+            // TODO use context
+            IEPEPlayer player = new IEPEPlayer();
+            context.setPlayer(player.getOutputStream());
+            RequestProcessor.Task task = RP.create(player);
+            progr = ProgressHandleFactory.createHandle("Simple task", task);
+            task.addTaskListener(new TaskListener() {
+                public void taskFinished(org.openide.util.Task task) {
+                    System.out.println("fin");
+                    progr.finish();
+                }
+            });
+            progr.start();
+            task.schedule(0);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+}
