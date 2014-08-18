fbf0734f75c68c29048fc394fe8718392e23906c
薛聿明 <102m05008@stud.sju.edu.tw>
2014-08-18 15:52:28 +0800

    Wizard new project.

---
+++ sju-eea.module-iepe-project/src/main/java/tw/edu/sju/ee/eea/module/iepe/project/IEPERealtimeAnalysisWizardPanel.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2014 Leo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package tw.edu.sju.ee.eea.module.iepe.project;
+
+import java.awt.Component;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.WizardValidationException;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+
+/**
+ * Panel just asking for basic info.
+ */
+public class IEPERealtimeAnalysisWizardPanel implements WizardDescriptor.Panel,
+        WizardDescriptor.ValidatingPanel, WizardDescriptor.FinishablePanel {
+
+    private WizardDescriptor wizardDescriptor;
+    private IEPERealtimeAnalysisPanelVisual component;
+
+    public IEPERealtimeAnalysisWizardPanel() {
+    }
+
+    public Component getComponent() {
+        if (component == null) {
+            component = new IEPERealtimeAnalysisPanelVisual(this);
+            component.setName(NbBundle.getMessage(IEPERealtimeAnalysisWizardPanel.class, "LBL_CreateProjectStep"));
+        }
+        return component;
+    }
+
+    public HelpCtx getHelp() {
+        return new HelpCtx(IEPERealtimeAnalysisWizardPanel.class);
+    }
+
+    public boolean isValid() {
+        getComponent();
+        return component.valid(wizardDescriptor);
+    }
+
+    private final Set<ChangeListener> listeners = new HashSet<ChangeListener>(1); // or can use ChangeSupport in NB 6.0
+
+    public final void addChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.add(l);
+        }
+    }
+
+    public final void removeChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.remove(l);
+        }
+    }
+
+    protected final void fireChangeEvent() {
+        Set<ChangeListener> ls;
+        synchronized (listeners) {
+            ls = new HashSet<ChangeListener>(listeners);
+        }
+        ChangeEvent ev = new ChangeEvent(this);
+        for (ChangeListener l : ls) {
+            l.stateChanged(ev);
+        }
+    }
+
+    public void readSettings(Object settings) {
+        wizardDescriptor = (WizardDescriptor) settings;
+        component.read(wizardDescriptor);
+    }
+
+    public void storeSettings(Object settings) {
+        WizardDescriptor d = (WizardDescriptor) settings;
+        component.store(d);
+    }
+
+    public boolean isFinishPanel() {
+        return true;
+    }
+
+    public void validate() throws WizardValidationException {
+        getComponent();
+        component.validate(wizardDescriptor);
+    }
+
+}
