d1a93cf268b17c81c7a680968c7c84e77ed7f2bf
薛聿明 <D10307009@mail.ntust.edu.tw>
2015-10-02 17:01:10 +0800

    Static Device OK.

---
+++ sju-eea.module-signal-project/src/main/java/tw/edu/sju/ee/eea/module/signal/device/DeviceChannel.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2015 D10307009
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package tw.edu.sju.ee.eea.module.signal.device;
+
+import java.awt.Color;
+import tw.edu.sju.ee.eea.module.signal.io.Channel;
+import tw.edu.sju.ee.eea.utils.io.tools.InputChannel;
+
+/**
+ *
+ * @author D10307009
+ */
+public class DeviceChannel implements Channel {
+
+    private InputChannel ic;
+    private String device;
+    private int channel;
+
+    public DeviceChannel(InputChannel ic, String device, int channel) {
+        this.ic = ic;
+        this.device = device;
+        this.channel = channel;
+    }
+
+    @Override
+    public String getName() {
+        return ic.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+    }
+
+    @Override
+    public String getDevice() {
+        return this.device;
+    }
+
+    @Override
+    public int getChannel() {
+        return this.channel;
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public void setColor(Color color) {
+        return;
+    }
+
+}
--- sju-eea.module-signal-project/src/main/java/tw/edu/sju/ee/eea/module/signal/device/SignalDeviceObject.java
+++ sju-eea.module-signal-project/src/main/java/tw/edu/sju/ee/eea/module/signal/device/SignalDeviceObject.java
@@ -4,7 +4,6 @@
  */
 package tw.edu.sju.ee.eea.module.signal.device;
 
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,26 +15,20 @@ import org.netbeans.core.api.multiview.MultiViews;
 import org.netbeans.spi.navigator.NavigatorLookupHint;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
-import org.openide.util.ImageUtilities;
-import org.openide.util.Lookup;
 import org.openide.windows.TopComponent;
-import tw.edu.sju.ee.eea.core.math.SineSimulator;
 import tw.edu.sju.ee.eea.jni.mps.MPS140801;
-import tw.edu.sju.ee.eea.module.iepe.channel.ChannelList;
-import tw.edu.sju.ee.eea.module.signal.oscillogram.ZoomRenderer;
-import tw.edu.sju.ee.eea.module.signal.temp.OscillogramChannel;
-import tw.edu.sju.ee.eea.module.signal.temp.SineInputStream;
+import tw.edu.sju.ee.eea.module.signal.io.ChannelList;
 import tw.edu.sju.ee.eea.utils.io.tools.EEAInput;
 
 /**
  *
  * @author 薛聿明
  */
-public class SignalDeviceObject extends AbstractNode implements tw.edu.sju.ee.eea.module.signal.io.ChannelList, NavigatorLookupHint, Serializable {
+public class SignalDeviceObject extends AbstractNode implements ChannelList<DeviceChannel>, NavigatorLookupHint, Serializable {
 
     public static final String MIMETYPE = "application/device";
     private TopComponent tc;
-    private ArrayList<OscillogramChannel> channels = new ArrayList<OscillogramChannel>();
+    private ArrayList<DeviceChannel> channels = new ArrayList<DeviceChannel>();
     private EEAInput input;
 
     public SignalDeviceObject(Project project) {
@@ -43,12 +36,16 @@ public class SignalDeviceObject extends AbstractNode implements tw.edu.sju.ee.ee
         setName("NI1234");
         setIconBaseWithExtension("tw/edu/sju/ee/eea/module/iepe/project/iepe_project.png");
         input = new EEAInput(new MPS140801(0, 16384));
-        channels.add(new OscillogramChannel("A", channels.size(), new ZoomRenderer(),
-                new SineInputStream(new SineSimulator(16384, 200, 5))));
+//        input.getIOChannel()
+//        channels.add(new OscillogramChannel("A", channels.size(), new ZoomRenderer(),
+//                new SineInputStream(new SineSimulator(16384, 200, 5))));
+        for (int i = 0; i < input.getIOChannel().length; i++) {
+            channels.add(new DeviceChannel(input.getIOChannel(i), "dd", i));
+        }
     }
 
     @Override
-    public List<OscillogramChannel> getChannels() {
+    public List<DeviceChannel> getChannels() {
         return channels;
     }
 
